20826d870ae93960991bd6d2b30283bf781b5fec
AM <am@omrt.com>
2023-07-18 13:30:10 -0700

    Day6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: HomePage(),
+      //home: const HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        fontFamily: GoogleFonts.abel().fontFamily,
+        //primaryTextTheme: GoogleFonts.lateefTextTheme()
+      ),
+      //debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     int days = 30;
     String name = "Flutter Tutorial";
     return Scaffold(
       appBar: AppBar(
-        title: Text("Training Series"),
+        title: Text("Training Sessions"),
       ),
       body: Center(
         // ignore: avoid_unnecessary_containers
@@ -17,6 +15,7 @@ class HomePage extends StatelessWidget {
           child: Text("Welcome to $name Series for $days days."),
         ),
       ),
+      drawer: Drawer(),
     );
   }
 }
